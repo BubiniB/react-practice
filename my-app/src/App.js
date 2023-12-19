dbe1383bd633eb6823c13ea29e9fae300ffcb8ea
BubiniB <cristina.ekqvist@outlook.com>
2023-12-19 09:51:55 +0000

    Learn how to bind events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+// import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreetingWithPrevState />
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
---
+++ my-app/src/components/EventBinding.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class StatefulGreeting extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    // handleClick() {
+    //     this.setState({
+    //         introduction: "Goodbye!",
+    //     });
+    //     console.log(this.state.introduction);
+    // }
+
+    handleClick = () => {
+        this.setState({
+            introduction: "Goodbye!",
+        });
+        console.log(this.state.introduction);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.introduction} {this.props.greeting}
+                </h1>
+                {/* <button onClick={() => this.handleClick()}> */}
+               {/* <button onClick={this.handleClick.bind(this)}> */} 
+               <button onClick={this.handleClick}>
+                    {this.state.buttonText}
+                </button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting;
+
+/*
+different ways to bind event handlers
+1. arrow function in the render method
+2. call bind on the handler with 'this'
+3. bind the event handler in the constructor
+4. define the handler as an arrow function
+*/
