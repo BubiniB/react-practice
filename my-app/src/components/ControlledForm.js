a42568ce26c91dfa74c761033028454892b07d2b
BubiniB <cristina.ekqvist@outlook.com>
2023-12-21 13:57:08 +0000

    Submitting form data

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -1,3 +1,10 @@
+// Learning outcomes:
+// - controlled form components
+// - work with different input types
+// - managing input field values
+// - data handling upon form submission
+
+
 import React, { Component } from 'react'
 
 export class ControlledForm extends Component {
@@ -5,7 +12,9 @@ export class ControlledForm extends Component {
       super(props)
     
       this.state = {
-         name: ""
+         name: "",
+         category: "website",
+         comments: "",
       }
     }
 
@@ -15,18 +24,67 @@ export class ControlledForm extends Component {
         })
     }
 
+    handleCategory = (event) => {
+        this.setState({
+            category: event.target.value
+        })
+    }
+
+    handleComments = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    /* Computed property name */
+
+    // handleChange = (event) => {
+    //     const {name, value} = event.target
+    //     this.setState({
+    //         [name]: value
+    //     })
+    // }
+
     render() {
         return (
         <div>
-            <form>
-                <label htmlFor="id-name">Your name:</label>
-                <input 
-                    value={this.state.name}
-                    onChange={this.handleNameChange} 
-                    id="id-name" 
-                    name="name" 
-                    type="text"
-                />
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label htmlFor="id-name">Your name:</label>
+                    <input 
+                        value={this.state.name}
+                        onChange={this.handleNameChange} 
+                        id="id-name" 
+                        name="name" 
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="id-category">Query category:</label>
+                    <select 
+                        id="id-category"
+                        name="category"
+                        value={this.state.category}
+                        onChange={this.handleCategory}
+                    >
+                        <option value="website">Website issue</option>
+                        <option value="order">Order issue</option>
+                        <option value="general">General inquiry</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="id-comments">Comments:</label>
+                    <textarea
+                        value={this.state.comments}
+                        onChange={this.handleComments} 
+                        id="id-comments" 
+                        name="comments"
+                    />
+                </div>
                 <input 
                     type="submit" 
                     value="Submit"
